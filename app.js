0ddec69fe4554826ce7765de76bb7d4527241b4b
Thomas Greenhalgh <thomas.greenhalgh@gmail.com>
2016-01-29 18:49:46 -0800

    adds custom service

--- app.js
+++ app.js
@@ -14,11 +14,30 @@ weatherApp.config(function($routeProvider) {
   })
 });
 
+//services - build a custom service so data can move from home page
+//to forecast page  (note: have to add service to controllers too)
+weatherApp.service('cityService', function() {
+  this.city = "Santa Monica, CA";
+})
+
 //controllers
-weatherApp.controller('homeController', ['$scope', function($scope) {
+weatherApp.controller('homeController',
+  ['$scope', 'cityService', function($scope, cityService) {
+
+    //set initial scope on the model
+    $scope.city = cityService.city;
+
+    //value will change via textbox, so update it in cityService too
+    $scope.$watch('city', function() {
+      cityService.city = $scope.city;
+    })
 
 }]);
 
-weatherApp.controller('forecastController', ['$scope', function($scope) {
+weatherApp.controller('forecastController',
+  ['$scope', 'cityService', function($scope, cityService) {
+
+    //set initial scope on the model
+    $scope.city = cityService.city;
 
 }]);
