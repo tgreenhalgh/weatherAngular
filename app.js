6ad404e032760f0301bf71ae77a665027bde40e3
Thomas Greenhalgh <thomas.greenhalgh@gmail.com>
2016-01-30 14:51:26 -0800

    adds option to change days forcast in URL

--- app.js
+++ app.js
@@ -12,6 +12,10 @@ weatherApp.config(function($routeProvider) {
     templateUrl: './pages/forecast.htm',
     controller: 'forecastController'
   })
+  .when('/forecast/:days', {
+    templateUrl: './pages/forecast.htm',
+    controller: 'forecastController'
+  })
 });
 
 //services - build a custom service so data can move from home page
@@ -33,16 +37,19 @@ weatherApp.controller('homeController', ['$scope', 'cityService', function($scop
 
 }]);
 
-weatherApp.controller('forecastController', ['$scope', '$resource', 'cityService', function($scope, $resource, cityService) {
+weatherApp.controller('forecastController', ['$scope', '$resource', '$routeParams', 'cityService', function($scope, $resource, $routeParams, cityService) {
 
     //set initial scope on the model
     $scope.city = cityService.city;
+
+    //checking routeParams - if no days passed, default to 2
+    $scope.days = $routeParams.days || 2;
 
     //go get data from my API
     $scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily?APPID=" + keys.api, {callback:"JSON_CALLBACK" }, {get: {method: "JSONP"}}) //JSON_CALLBACK and JSONP are so can
     //download the API call (i.e. not a hack attempt)
 
-    $scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, cnt: 2 })
+    $scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, cnt: $scope.days })
 
     $scope.convertToFahrenheit = function(degK) {
       return Math.round((1.8 * (degK - 273)) + 32);
