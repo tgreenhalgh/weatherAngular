6bd3cd1bcef7b0f68faf1043c4b67c4c7c731016
Thomas Greenhalgh <thomas.greenhalgh@gmail.com>
2016-01-30 16:35:06 -0800

    adds ng-submit (can hit return in box to submit)

--- controllers.js
+++ controllers.js
@@ -1,23 +1,42 @@
-weatherApp.controller('forecastController', ['$scope', '$resource', '$routeParams', 'cityService', function($scope, $resource, $routeParams, cityService) {
+//controllers
+weatherApp.controller('homeController', ['$scope', '$location', 'cityService', function($scope, $location, cityService) {
 
     //set initial scope on the model
     $scope.city = cityService.city;
 
-    //checking routeParams - if no days passed, default to 2
-    $scope.days = $routeParams.days || '1'; //make 1 a string so ng-class in forecast works
+    //value will change via textbox, so update it in cityService too
+    $scope.$watch('city', function() {
+      cityService.city = $scope.city;
+    });
 
-    //go get data from my API
-    $scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily?APPID=" + keys.api, {callback:"JSON_CALLBACK" }, {get: {method: "JSONP"}}) //JSON_CALLBACK and JSONP are so can
-    //download the API call (i.e. not a hack attempt)
+    $scope.submit = function() {
+      //injected $locatiion
+      $location.path("/forecast")
+    };
 
-    $scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, cnt: $scope.days })
+}]);
 
-    $scope.convertToFahrenheit = function(degK) {
-      return Math.round((1.8 * (degK - 273)) + 32);
-    }
+weatherApp.controller('forecastController', ['$scope', '$resource', '$routeParams', 'cityService', function($scope, $resource, $routeParams, cityService) {
 
-    $scope.convertToDate = function(dt) {
-      return new Date(dt * 1000); //API gave back ms
-    }
+  //set initial scope on the model
+  $scope.city = cityService.city;
+
+  //checking routeParams - if no days passed, default to 2
+  $scope.days = $routeParams.days || '1'; //make 1 a string so ng-class in forecast works
+
+  //go get data from my API
+  $scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily?APPID=" + keys.api, {callback:"JSON_CALLBACK" }, {get: {method: "JSONP"}});
+  //JSON_CALLBACK and JSONP are so can
+  //download the API call (i.e. not a hack attempt)
+
+  $scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, cnt: $scope.days });
+
+  $scope.convertToFahrenheit = function(degK) {
+    return Math.round((1.8 * (degK - 273)) + 32);
+  };
+
+  $scope.convertToDate = function(dt) {
+    return new Date(dt * 1000); //API gave back ms so * 1000
+  };
 
 }]);
--- services.js
+++ services.js
@@ -2,17 +2,4 @@
 //to forecast page  (note: have to add service to controllers too)
 weatherApp.service('cityService', function() {
   this.city = "Santa Monica, CA";
-})
-
-//controllers
-weatherApp.controller('homeController', ['$scope', 'cityService', function($scope, cityService) {
-
-    //set initial scope on the model
-    $scope.city = cityService.city;
-
-    //value will change via textbox, so update it in cityService too
-    $scope.$watch('city', function() {
-      cityService.city = $scope.city;
-    })
-
-}]);
+});
