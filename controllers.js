5dc41a694bd39a3f200682bb3bf7b77b42b2cf05
Thomas Greenhalgh <thomasgreenhalgh@gmail.com>
2016-01-31 10:13:00 -0800

    move API call from controller to service

--- controllers.js
+++ controllers.js
@@ -16,7 +16,7 @@ weatherApp.controller('homeController', ['$scope', '$location', 'cityService', f
 
 }]);
 
-weatherApp.controller('forecastController', ['$scope', '$resource', '$routeParams', 'cityService', function($scope, $resource, $routeParams, cityService) {
+weatherApp.controller('forecastController', ['$scope', '$routeParams', 'cityService', 'weatherService', function($scope, $routeParams, cityService, weatherService) {
 
   //set initial scope on the model
   $scope.city = cityService.city;
@@ -24,12 +24,7 @@ weatherApp.controller('forecastController', ['$scope', '$resource', '$routeParam
   //checking routeParams - if no days passed, default to 2
   $scope.days = $routeParams.days || '1'; //make 1 a string so ng-class in forecast works
 
-  //go get data from my API
-  $scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily?APPID=" + keys.api, {callback:"JSON_CALLBACK" }, {get: {method: "JSONP"}});
-  //JSON_CALLBACK and JSONP are so can
-  //download the API call (i.e. not a hack attempt)
-
-  $scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, cnt: $scope.days });
+  $scope.weatherResult = weatherService.GetWeather($scope.city, $scope.days);
 
   $scope.convertToFahrenheit = function(degK) {
     return Math.round((1.8 * (degK - 273)) + 32);
--- services.js
+++ services.js
@@ -3,3 +3,21 @@
 weatherApp.service('cityService', function() {
   this.city = "Santa Monica, CA";
 });
+
+// weatherApp.service('weatherService', function() {
+// need to inject $resource service into our custom service
+weatherApp.service('weatherService', ['$resource', function($resource) {
+  //add a method
+  this.GetWeather = function(city, days) {
+    //go get data from my API
+    //
+    // $scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily?APPID=" + keys.api, {callback:"JSON_CALLBACK" }, {get: {method: "JSONP"}});
+    //JSON_CALLBACK and JSONP are so can
+    //download the API call (i.e. not a hack attempt)
+    //
+    var weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily?APPID=" + keys.api, {callback:"JSON_CALLBACK" }, {get: {method: "JSONP"}});
+
+    // $scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, cnt: $scope.days });
+    return weatherResult = weatherAPI.get({ q: city, cnt: days });
+  };
+}]);
